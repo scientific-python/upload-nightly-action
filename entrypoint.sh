6c38d4ec963cdf27efc1cb070ebc306370bf7983
Martin Fleischmann <martin@martinfleischmann.net>
2023-05-22 12:13:59 -0700

    install in base

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,7 @@ set -x
 ANACONDA_ORG="scientific-python-nightly-wheel"
 ANACONDA_TOKEN="$ANACONDA_NIGHTLY_UPLOAD_TOKEN"
 
-
-export PATH=$CONDA/bin:$PATH
-micromamba create -n upload -y python anaconda-client -c conda-forge
-micromamba activate upload
+micromamba install python anaconda-client -c conda-forge
 
 anaconda -t $ANACONDA_TOKEN upload --force -u $ANACONDA_ORG $ARTIFACTS_PATH/*.whl
 echo "Index: https://pypi.anaconda.org/$ANACONDA_ORG/simple"

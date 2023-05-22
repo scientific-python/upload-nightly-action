5f778b65f692edff3caa9c2e791cbbf11e6c3191
Martin Fleischmann <martin@martinfleischmann.net>
2023-05-22 12:04:35 -0700

    use mnicromamba instead of conda

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,8 @@ ANACONDA_TOKEN="$ANACONDA_NIGHTLY_UPLOAD_TOKEN"
 
 
 export PATH=$CONDA/bin:$PATH
-conda create -n upload -y python=3.10
-source activate upload
-conda install -y anaconda-client
+mnicromamba create -n upload -y python=3.10 anaconda-client
+mnicromamba activate upload
 
 anaconda -t $ANACONDA_TOKEN upload --force -u $ANACONDA_ORG $ARTIFACTS_PATH/*.whl
 echo "Index: https://pypi.anaconda.org/$ANACONDA_ORG/simple"

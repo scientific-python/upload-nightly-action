cce130f1c8e3c9ffd07d91fe9c414c588366485a
Martin Fleischmann <martin@martinfleischmann.net>
2023-05-22 12:04:58 -0700

    typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ ANACONDA_TOKEN="$ANACONDA_NIGHTLY_UPLOAD_TOKEN"
 
 
 export PATH=$CONDA/bin:$PATH
-mnicromamba create -n upload -y python=3.10 anaconda-client
-mnicromamba activate upload
+micromamba create -n upload -y python=3.10 anaconda-client
+micromamba activate upload
 
 anaconda -t $ANACONDA_TOKEN upload --force -u $ANACONDA_ORG $ARTIFACTS_PATH/*.whl
 echo "Index: https://pypi.anaconda.org/$ANACONDA_ORG/simple"

b7703d00fe268a8df8d6c04a4e04e49272a69655
Martin Fleischmann <martin@martinfleischmann.net>
2023-05-22 12:25:50 -0700

    init shell

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ ANACONDA_ORG="scientific-python-nightly-wheel"
 ANACONDA_TOKEN="$ANACONDA_NIGHTLY_UPLOAD_TOKEN"
 
 micromamba install -y -n base python anaconda-client -c conda-forge
+eval "$(micromamba shell hook --shell=)"
 micromamba activate base
 
 anaconda -t $ANACONDA_TOKEN upload --force -u $ANACONDA_ORG $ARTIFACTS_PATH/*.whl

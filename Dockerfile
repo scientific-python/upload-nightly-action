b7d4502a0dc9a9acce3b8ea96b87befb4e9cf825
Matthias Bussonnier <bussonniermatthias@gmail.com>
2023-05-22 11:04:12 -0700

    test

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM ubuntu:latest
+
+RUN apt-get update && \
+    apt-get install -y python3.10 python3-pip
+
+RUN python3.10 -m pip install --upgrade pip && \
+    pip install anaconda-client
+
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -x
+
+ANACONDA_ORG="scientific-python-nightly-wheel"
+ANACONDA_TOKEN="$NIGHTLY_UPLOAD_TOKEN"
+
+
+export PATH=$CONDA/bin:$PATH
+conda create -n upload -y python=3.10
+source activate upload
+conda install -y anaconda-client
+
+anaconda -t $ANACONDA_TOKEN upload --force -u $ANACONDA_ORG $ARTIFACTS_PATH/*
+echo "Index: https://pypi.anaconda.org/$ANACONDA_ORG/simple"
